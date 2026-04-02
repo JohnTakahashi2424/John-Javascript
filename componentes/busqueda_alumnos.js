cf957ae5b7bf51092d1f33ec4d3acd6d9e54b9b3
JohnTakahashi2424 <Takahashijohnxd01@gmail.com>
2026-04-02 10:54:58 -0600

    migrar persistencia local a fetch sincronizado

---
+++ componentes/busqueda_alumnos.js
@@ -0,0 +1,71 @@
+const busqueda_alumnos = {
+    data(){
+        return{
+            buscar:'',
+            alumnos:[]
+        }
+    },
+    methods:{
+        modificarAlumno(alumno){
+            this.$emit('modificar', alumno);
+        },
+        async obtenerAlumnos(){
+            fetch(`private/modulos/alumnos/alumno.php?accion=consultar&buscar=${this.buscar}`)
+                .then(response=>response.json())
+                .then(data=>{
+                    this.alumnos = data;
+                });
+        },
+        async eliminarAlumno(alumno, e){
+            e.stopPropagation();
+            alertify.confirm('Elimanar alumnos', `¿Está seguro de eliminar el alumno ${alumno.nombre}?`, async e=>{
+                fetch(`private/modulos/alumnos/alumno.php?accion=eliminar&alumnos=${JSON.stringify(alumno)}`)
+                    .then(response=>response.json())
+                    .then(data=>{
+                        if(data!=true) alertify.error(`Error al sincronizar con el servidor: ${data}`);
+                    });
+                this.obtenerAlumnos();
+                alertify.success(`Alumno ${alumno.nombre} eliminado correctamente`);
+            }, () => {
+                //No hacer nada
+            });
+        },
+    },
+    template: `
+        <div class="row">
+            <div class="col-6">
+                <table class="table table-striped table-hover" id="tblAlumnos">
+                    <thead>
+                        <tr>
+                            <th colspan="6">
+                                <input autocomplete="off" type="search" @keyup="obtenerAlumnos()" v-model="buscar" placeholder="Buscar alumno" class="form-control">
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>CODIGO</th>
+                            <th>NOMBRE</th>
+                            <th>DIRECCION</th>
+                            <th>EMAIL</th>
+                            <th>TELEFONO</th>
+                            <th>HASH</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="alumno in alumnos" :key="alumno.idAlumno" @click="modificarAlumno(alumno)">
+                            <td>{{ alumno.codigo }}</td>
+                            <td>{{ alumno.nombre }}</td>
+                            <td>{{ alumno.direccion }}</td>
+                            <td>{{ alumno.email }}</td>
+                            <td>{{ alumno.telefono }}</td>
+                            <td>{{ alumno.hash }}</td>
+                            <td>
+                                <button class="btn btn-danger" @click="eliminarAlumno(alumno, $event)">DEL</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    `
+};
