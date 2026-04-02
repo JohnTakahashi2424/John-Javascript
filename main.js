207a952bfa7f00b92e1f3bd12f6aa60cc87024c6
JohnTakahashi2424 <Takahashijohnxd01@gmail.com>
2026-04-02 10:54:15 -0600

    eliminar dexie cdn e inicializacion de base local

---
+++ main.js
@@ -0,0 +1,45 @@
+const { createApp } = Vue,
+    sha256 = CryptoJS.SHA256;
+
+
+createApp({
+    components:{
+        alumnos,
+        busqueda_alumnos,
+        materias,
+        busqueda_materias,
+        docentes,
+        busqueda_docentes,
+        matriculas,
+        busqueda_matriculas,
+        inscripciones,
+        busqueda_inscripciones
+    },
+    data(){
+        return{
+            forms:{
+                alumnos:{mostrar:false},
+                busqueda_alumnos:{mostrar:false},
+                materias:{mostrar:false},
+                busqueda_materias:{mostrar:false},
+                docentes:{mostrar:false},
+                busqueda_docentes:{mostrar:false},
+                matriculas:{mostrar:false},
+                busqueda_matriculas:{mostrar:false},
+                inscripciones:{mostrar:false},
+                busqueda_inscripciones:{mostrar:false}
+            }
+        }
+    },
+    methods:{
+        buscar(ventana, metodo){
+            this.$refs[ventana][metodo]();
+        },
+        abrirVentana(ventana){
+            this.forms[ventana].mostrar = !this.forms[ventana].mostrar;
+        },
+        modificar(ventana, metodo, data){
+            this.$refs[ventana][metodo](data);
+        }
+    }
+}).mount("#app");
